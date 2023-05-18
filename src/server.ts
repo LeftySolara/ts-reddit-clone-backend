ae51972dafe912ee41ce8abf165ca939e3536ddd
Julianne Adams <julianne@julianneadams.info>
2023-05-18 16:49:51 -0400

    Add: pino and CORS setup for express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response, NextFunction } from "express";
+import bodyParser, { urlencoded } from "body-parser";
+import { pinoHttp } from "pino-http";
 import { appConfig } from "@utils/appConfig";
 import logger from "@utils/logger";
 
 const app = express();
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
+app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+
+app.use(pinoHttp({ logger }));
+
+/* Add CORS headers to all responses. */
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    appConfig.express.corsOrigin as string,
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/", (req: Request, res: Response) => {
   logger.info("Received request...");
   return res.json({ message: "Hello World!" });
 });

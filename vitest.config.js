49105ff7aa1e018e0fa992aecad72d0918307f51
Julianne Adams <julianne@julianneadams.info>
2023-05-25 12:55:33 -0400

    Make: switch to vitest

---
+++ vitest.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: "@infra", replacement: path.resolve(__dirname, "src/infra") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@utils",
+        replacement: path.resolve(__dirname, "src/utils"),
+      },
+    ],
+  },
+});
